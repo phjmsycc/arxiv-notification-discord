f8aefc10cd51c5fdd55a26bbd30eda76eef37317
Minoru Kanega <phys_chibaraki@yahoo.co.jp>
2024-08-07 19:06:35 +0900

    feat: Add debug statement to search_keyword function in paper_arxiv.py

--- paper_arxiv.py
+++ paper_arxiv.py
@@ -66,6 +66,7 @@ def search_keyword(
         abstract = article.summary
         score, hit_keywords = calc_score(abstract, keywords)
         if (score != 0) and (score >= score_threshold):
+            print(article) # debug
             results.append(article)
             key.append(hit_keywords)
             scores.append(score)
